c51015c31328ec2faec037a2bd2bcecabf5c36ab
Kazunori Jo <519604+itochan@users.noreply.github.com>
2024-12-17 23:11:29 +0900

    Implement feature to get friend information

---
+++ redeem.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "csv"
+require "digest"
+require "faraday"
+require "json"
+
+SECRET = "tB87#kPtkxqOS2"
+
+def generate_sign(fid, time)
+  Digest::MD5.hexdigest("fid=#{fid}&time=#{time}#{SECRET}")
+end
+
+if ARGV.length < 2
+  puts "Usage: ruby redeemer.rb player [csv_file]"
+  exit
+end
+
+csv_file = ARGV[1]
+
+rows = CSV.read(csv_file, headers: true)
+updated_rows = []
+
+rows.each do |row|
+  fid = row["fid"]
+  time = Time.now.to_i
+  sign = generate_sign(fid, time)
+
+  response = Faraday.post("https://wos-giftcode-api.centurygame.com/api/player", {
+    fid: fid,
+    time: time,
+    sign: sign
+  })
+
+  begin
+    data = JSON.parse(response.body)
+    if data["code"] == 0
+      updated_rows << [
+        data["data"]["fid"],
+        data["data"]["nickname"],
+        data["data"]["kid"],
+        data["data"]["stove_lv"],
+        data["data"]["total_recharge_amount"]
+      ]
+      puts "Success for fid: #{fid} nickname: #{data["data"]["nickname"]}"
+    else
+      puts "Failed for fid: #{fid} code: #{data["code"]} message: #{data["message"]}"
+      updated_rows << row.to_h.values
+    end
+  rescue => e
+    puts "Failed for fid: #{fid}"
+    puts e
+    puts response.body
+    updated_rows << row.to_h.values
+  end
+
+  sleep(rand(1.0..2.0))
+end
+
+CSV.open(csv_file, "wb", write_headers: true, headers: rows.headers) do |csv_out|
+  updated_rows.each do |updated_row|
+    csv_out << updated_row
+  end
+end
+
+puts "Processing completed."
